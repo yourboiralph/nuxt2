0b728f6907dc57f5aca367d73d8d60c5aa8762cf
yourboiralph <126213216+yourboiralph@users.noreply.github.com>
2023-05-19 08:35:04 +0530

    Add files via upload

---
+++ components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card text-center">
+        <img :src="product.image" alt="product thumb">
+
+    </div>
+</template>
+
+<script setup>
+    const { product } = defineProps(['product'])
+
+</script>
+
+<style scoped>
+ 
+</style>
